8fa16251e0e62358bbd6fc03095c105096da70dd
JulieHuang <huangxj1993@gmail.com>
2024-11-10 17:57:40 +0100

    ft_lstnew.c added

---
+++ ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xhuang <xhuang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 17:46:29 by xhuang            #+#    #+#             */
+/*   Updated: 2024/11/10 17:55:35 by xhuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+	{
+		return (NULL);
+	}
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
+}
